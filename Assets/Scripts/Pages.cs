6769f1ca39027cff046a9d278e4a27a6cac845a4
HappyIRL <59649664+HappyIRL@users.noreply.github.com>
2020-03-28 15:19:15 +0100

    stuff

--- Assets/Scripts/Pages.cs
+++ Assets/Scripts/Pages.cs
@@ -2,21 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
 
 public class Pages : MonoBehaviour
 {
 	[SerializeField] private Text _title;
 	[SerializeField] private Text _description;
 	[SerializeField] private Image _logo;
+
+	[SerializeField] private ScrollSnapBase _scrollSnapBase;
+
+
 	private void Start()
 	{
 
 	}
 
-	public void SetPageInfo(Image logo,Text title, Text description)
+	public void SetPageInfo(AppInfo appinfo)
 	{
-		_title = title;
-		_description = description;
-		_logo = logo;
+		AppInfo name = appinfo;
+		_title.text = name.Title;
+		//_logo.sprite = name.Logo;
+		_description.text = name.Description;
 	}
+
+	public void SubscribeOnPageChange(UnityEngine.Events.UnityAction<int> action)
+	{
+		_scrollSnapBase.OnSelectionChangeEndEvent.AddListener(action);
+	}
+
 }
