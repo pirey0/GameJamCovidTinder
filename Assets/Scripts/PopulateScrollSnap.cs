b56241d610a73e3c21d1cae761cb701d74acdeb4
HappyIRL <59649664+HappyIRL@users.noreply.github.com>
2020-03-28 13:36:00 +0100

    start

---
+++ Assets/Scripts/PopulateScrollSnap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateScrollSnap : MonoBehaviour
+{
+	private void Start()
+	{
+		AddSliderOnStart();
+	}
+	private void Update()
+	{
+
+	}
+
+	public void AddSlider()
+	{
+		//adds a random slider into a list
+		//if the list is bigger than 2 elements, call RemoveSlider
+	}
+
+	private void RemoveSlider()
+	{
+		//Removes slider at index 0
+	}
+
+	private void AddSliderOnStart()
+	{
+		//adds 2 random sliders on start
+	}
+	
+}
